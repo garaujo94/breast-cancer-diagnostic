08ca5abd9ab4f7f5a52b8e28a33a671aacb0e9c7
Gustavo Araujo <gala.gustavoantonio@gmail.com>
2021-02-20 15:45:54 -0300

    adding containers for flask and streamlit

--- app-flask.py
+++ app-flask.py
@@ -1,1 +1,17 @@
-#This file will be a flask app
+import numpy as np
+from flask import Flask, request, jsonify
+import pickle
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def verifica_api_online():
+  return "API ONLINE v1.0st", 200
+
+
+#
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
---
+++ app-streamlit.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.write('''
+# Hello World!
+Future deploy app with streamlit
+''')
